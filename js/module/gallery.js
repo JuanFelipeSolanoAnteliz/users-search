b2ca7bbb3c88f5e670354cb8eb2f708d8cccfef9
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-06-20 23:19:12 -0500

    feat: :construction: filtros de busqueda

---
+++ js/module/gallery.js
@@ -0,0 +1,21 @@
+export const usergallery = async(info)=>{
+    let plantilla = '';
+    info.forEach(element => {
+        // console.log(element.avatar)
+       plantilla +=/*html*/`
+       <section id="main__section" class="main__section" >
+       <article class="user__section" >
+           <div class="user__image__container">
+               <img id="user__image" src="${element.avatar}" >
+           </div>
+           <div class="user__data__viwer" >
+               <h2 id="user__name">${element.name_full}</h2>
+               <span id="user__description" >${element.description}</span>
+           </div>
+       </article>
+       <span class="span"></span>
+   </section>` 
+    }); 
+    return plantilla;
+    
+};
